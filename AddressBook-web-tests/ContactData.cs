c52a89d7533af534e31262a15c07428003460518
Pavel Khlusenkov <pkhlusenkov@tula.co>
2022-10-13 01:06:20 +0400

    AddNewContactTest

---
+++ AddressBook-web-tests/ContactData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_web_tests
+{
+    internal class ContactData
+    {
+        
+        private string firstname;
+        private string lastname = "";
+
+        public ContactData(string name)
+        {
+            this.firstname = name;
+        }
+        public string FirstName
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+    }
+}
