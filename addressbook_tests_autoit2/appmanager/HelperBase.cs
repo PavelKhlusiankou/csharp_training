db7bcb240ad13e098699ca84b60ac2ca1cd8f3af
Pavel Khlusenkov <pkhlusenkov@tula.co>
2022-11-21 02:35:09 +0400

    Задание №18 версия 1

---
+++ addressbook_tests_autoit2/appmanager/HelperBase.cs
@@ -0,0 +1,19 @@
+﻿using AutoItX3Lib;
+using NUnit.Framework.Internal;
+
+namespace addressbook_tests_autoit2
+{
+    public class HelperBase
+    {
+        public ApplicationManager manager;
+        protected string WINTITLE;
+        protected AutoItX3 aux;
+
+        public HelperBase(ApplicationManager manager)
+        {
+            this.manager = manager;
+            this.aux = manager.Aux;
+            WINTITLE = ApplicationManager.WINTITLE;
+        }
+    }
+}
---
+++ addressbook_tests_autoit2/tests/GroupRemovalTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Concurrent;
+
+
+namespace addressbook_tests_autoit2
+{
+    [TestFixture]
+    public class GroupRemovalTestss : TestBase
+    {
+        [Test]
+        public void TestGroupRemove()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            app.Groups.Remove();
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            newGroups.RemoveAt(0);
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
+
